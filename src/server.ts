ee36325594441f7e1e00083ef4dd56f5f79a5f01
MateuxLucax <mateuxlucax@gmail.com>
2022-05-08 17:12:07 -0300

    feat: added DB_HOST and CORS_ORIGIN env variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
-import { PORT, HOSTNAME } from "./utils/environmentUtil"
+import { PORT, HOSTNAME, CORS_ORIGIN } from "./utils/environmentUtil"
 import routes from "./routes"
 
 const app = express()
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use(cors({
-    origin: ["http://localhost:3000"]
+    origin: CORS_ORIGIN
 }))
 
 app.listen(PORT, () => {
